5c092b80841365955dec261ad4b495e873aa08a5
Skymqq <1538974565@qq.com>
2026-06-04 15:08:22 +0800

    fix: make pages dynamic to avoid prerender error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 import TodoList from "@/components/todo-list";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const supabase = await createClient();
   const {
